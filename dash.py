fd16c785e62379dd26d7f05f399abafc49cb0edd
ritika-2310 <bhasinritika561@gmail.com>
2025-10-15 19:28:02 +0530

    Update dash.py

--- dash.py
+++ dash.py
@@ -94,67 +94,67 @@ if uploaded_file is not None:
  previous_revenue = yearly_revenue.get(prev_year, 0)
  growth_percent = ((latest_revenue - previous_revenue) / previous_revenue * 100) if previous_revenue != 0 else 0
 
-#sidebar filters
-st.sidebar.header("filters")
-year_list = sorted(df['year'].dropna().unique())
-selected_year = st.sidebar.selectbox("select year",options = ["all"]+[str(y) for y in year_list])
-if selected_year!="all":
+ #sidebar filters
+ st.sidebar.header("filters")
+ year_list = sorted(df['year'].dropna().unique())
+ selected_year = st.sidebar.selectbox("select year",options = ["all"]+[str(y) for y in year_list])
+ if selected_year!="all":
     df = df[df['year']==int(selected_year)]
-month_list = sorted(df['month_name'].dropna().unique())
-selected_month = st.sidebar.selectbox("select month",options = ["all"]+[str(y) for y in month_list])
-if selected_month!="all":
+ month_list = sorted(df['month_name'].dropna().unique())
+ selected_month = st.sidebar.selectbox("select month",options = ["all"]+[str(y) for y in month_list])
+ if selected_month!="all":
     df = df[df['month_name']==selected_month]
-total_revenue = df['revenue'].sum()
-total_orders = len(df)
-avg_order_value = total_revenue/total_orders if total_orders > 0 else 0
+ total_revenue = df['revenue'].sum()
+ total_orders = len(df)
+ avg_order_value = total_revenue/total_orders if total_orders > 0 else 0
 
-#metrics
-col1,col2,col3,col4 = st.columns(4)
-col1.metric("total revenue",f"${total_revenue:,.2f}")
-col2.metric("total orders",total_orders)
-col3.metric("average value order",f"${avg_order_value:,.2f}")
-col4.metric("yearly growth",f"{growth_percent:.2f}%")
+ #metrics
+ col1,col2,col3,col4 = st.columns(4)
+ col1.metric("total revenue",f"${total_revenue:,.2f}")
+ col2.metric("total orders",total_orders)
+ col3.metric("average value order",f"${avg_order_value:,.2f}")
+ col4.metric("yearly growth",f"{growth_percent:.2f}%")
 
-st.sidebar.subheader("filter by date range")
-min_date = pd.to_datetime(df['Order Date']).min()
-max_date = pd.to_datetime(df['Order Date']).max()
-# Convert to Python date, with fallback if NaT
-if pd.isna(min_date):
+ st.sidebar.subheader("filter by date range")
+ min_date = pd.to_datetime(df['Order Date']).min()
+ max_date = pd.to_datetime(df['Order Date']).max()
+ # Convert to Python date, with fallback if NaT
+ if pd.isna(min_date):
     min_date = date.today()
-else:
+ else:
     min_date = min_date.date()
 
-if pd.isna(max_date):
+ if pd.isna(max_date):
     max_date = date.today()
-else:
+ else:
     max_date = max_date.date()
 
-date_range = st.sidebar.date_input("select date range",value=(min_date,max_date),min_value = min_date,max_value = max_date) #use tuple not list
-# Safely unpack
-if isinstance(date_range, tuple) and len(date_range) == 2:
+ date_range = st.sidebar.date_input("select date range",value=(min_date,max_date),min_value = min_date,max_value = max_date) #use tuple not list
+ # Safely unpack
+ if isinstance(date_range, tuple) and len(date_range) == 2:
     start_date, end_date = date_range
-else:
+ else:
     start_date = end_date = date_range
-df = df[(pd.to_datetime(df['Order Date'])>=pd.to_datetime(start_date)) & (pd.to_datetime(df['Order Date'])<=pd.to_datetime(end_date))]
+ df = df[(pd.to_datetime(df['Order Date'])>=pd.to_datetime(start_date)) & (pd.to_datetime(df['Order Date'])<=pd.to_datetime(end_date))]
 
-st.markdown("-----export filtered data")
-csv = df.to_csv(index=False).encode('utf-8') #convert to csv
-#convert to excel
-excel_buffer = BytesIO()
-with pd.ExcelWriter(excel_buffer,engine='xlsxwriter') as writer:
+ st.markdown("-----export filtered data")
+ csv = df.to_csv(index=False).encode('utf-8') #convert to csv
+ #convert to excel
+ excel_buffer = BytesIO()
+ with pd.ExcelWriter(excel_buffer,engine='xlsxwriter') as writer:
     df.to_excel(writer,index=False,sheet_name='filtered_data')
-excel_data = excel_buffer.getvalue()
+ excel_data = excel_buffer.getvalue()
 
-#download buttons
-col_a, col_b = st.columns(2)
-with col_a:
+ #download buttons
+ col_a, col_b = st.columns(2)
+ with col_a:
     st.download_button(
         label = "download as CSV",
         data = csv,
         file_name='filtered_data.csv',
         mime='csv/text'
     )
-with col_b:
+ with col_b:
     st.download_button(
         label = "download as xlsx",
         data = excel_data,
@@ -162,10 +162,10 @@ with col_b:
         mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' #
     )
 
-#charts to be made in 3 different tabs
-tab1,tab2,tab3 = st.tabs(['monthly trend','top products','state wise revenue'])
-#monthly trend
-with tab1:
+ #charts to be made in 3 different tabs
+ tab1,tab2,tab3 = st.tabs(['monthly trend','top products','state wise revenue'])
+ #monthly trend
+ with tab1:
     monthly = df.groupby('month_name')['revenue'].sum().reindex(month_list)
     fig, ax = plt.subplots(figsize=(8,4))
     ax.plot(monthly.index,monthly.values,marker='o')
@@ -182,7 +182,7 @@ with tab1:
         data = buf.getvalue(),
         mime='image/png'
     )
-with tab2:
+ with tab2:
     top_products = df.groupby('Product Name')['revenue'].sum().sort_values(ascending=False).head(10)
     fig,ax = plt.subplots(figsize=(8,5))
     ax.barh(top_products.index[::-1],top_products.values[::-1])
@@ -197,7 +197,7 @@ with tab2:
         data = buf.getvalue(),
         mime='image/png'
     )
-with tab3:
+ with tab3:
     state_rev = df.groupby('State')['revenue'].sum().sort_values(ascending=False).head(10)
     fig,ax = plt.subplots(figsize=(8,5))
     ax.bar(state_rev.index,state_rev.values)
@@ -214,7 +214,6 @@ with tab3:
         mime='image/png'
     )
 
-st.markdown("----")
+ st.markdown("----")
 
-st.caption("<div style='text-align:center;color:gray;'>""© 2025 Retail Sales Analytics Built with Streamlit & MySQL""</div>",unsafe_allow_html=True)
-
+ st.caption("<div style='text-align:center;color:gray;'>""© 2025 Retail Sales Analytics Built with Streamlit & MySQL""</div>",unsafe_allow_html=True)
